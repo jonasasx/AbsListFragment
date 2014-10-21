511152aa1014351e82d6810a4e853d18818cda90
jonasas <jonasasx@gmail.com>
2014-10-21 13:37:37 +0400

    test fix

--- src/com/jonasasx/list/WorkFragment.java
+++ src/com/jonasasx/list/WorkFragment.java
@@ -3,7 +3,6 @@ package com.jonasasx.list;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
@@ -17,50 +16,53 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
 public class WorkFragment extends AbsListFragment implements Callback {
-	private List<String>	mDataList	= new ArrayList<String>();
-	private BaseAdapter		mAdapter	= new BaseAdapter() {
-											@Override
-											public int getCount() {
-												return mDataList.size();
-											}
+	private List<String> mDataList = new ArrayList<String>();
+	private BaseAdapter mAdapter = new BaseAdapter() {
+		@Override
+		public int getCount() {
+			return mDataList.size();
+		}
 
-											@Override
-											public String getItem(int position) {
-												return mDataList.get(position);
-											}
+		@Override
+		public String getItem(int position) {
+			return mDataList.get(position);
+		}
 
-											@Override
-											public long getItemId(int position) {
-												return 0;
-											}
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
 
-											@Override
-											public View getView(int position, View convertView, ViewGroup parent) {
-												ViewHolder holder;
-												if (convertView == null) {
-													holder = new ViewHolder();
-													convertView = new FrameLayout(getActivity());
-													AbsListView.LayoutParams params = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.MATCH_PARENT);
-													convertView.setLayoutParams(params);
-													holder.text = new TextView(getActivity());
-													holder.text.setPadding(12, 32, 12, 32);
-													convertView.setBackgroundResource(R.drawable.list_item_bg);
-													((FrameLayout) convertView).addView(holder.text);
-													convertView.setTag(holder);
-												} else {
-													holder = (ViewHolder) convertView.getTag();
-												}
-												holder.position = position;
-												holder.text.setText(getItem(position));
-												return convertView;
-											}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = new FrameLayout(getActivity());
+				AbsListView.LayoutParams params = new AbsListView.LayoutParams(
+						AbsListView.LayoutParams.MATCH_PARENT,
+						AbsListView.LayoutParams.MATCH_PARENT);
+				convertView.setLayoutParams(params);
+				holder.text = new TextView(getActivity());
+				holder.text.setPadding(12, 32, 12, 32);
+				convertView.setBackgroundResource(R.drawable.list_item_bg);
+				((FrameLayout) convertView).addView(holder.text);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			holder.position = position;
+			holder.text.setText(getItem(position));
+			return convertView;
+		}
 
-											class ViewHolder {
-												int			position	= -1;
-												TextView	text;
-											}
-										};
-	protected int			n			= 0;
+		class ViewHolder {
+			@SuppressWarnings("unused")
+			int position = -1;
+			TextView text;
+		}
+	};
+	protected int n = 0;
 
 	public WorkFragment() {
 		setFromBottom(false);
